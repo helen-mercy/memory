a7211c7579348af2bdcaa4dccb1a8cd3a5cedd7d
helen-mercy <helenarackal@gmail.com>
2025-09-13 20:24:00 +0530

    new

---
+++ cards.js
@@ -0,0 +1,38 @@
+var score=0;
+var cards=[
+    "A","E","F" ,"H","K","R","T","X","Y","Z"
+]
+var cardSet;
+var board=[];
+
+window.onload=function(){
+    shuffleCards();
+    startGame();
+}
+function shuffleCards() {
+    cardSet=cards.concat(cards);
+    console.log(cardSet);
+    for(let i=0;i<cardSet.length;i++){
+        let j=Math.floor(Math.random()*cardSet.length);
+        let temp=cardSet[i];
+        cardSet[i]=cardSet[j];
+        cardSet[j]=temp;
+    }
+    console.log(cardSet);
+}
+function startGame(){
+    for(let r=0;r<rows;r++){
+        let row=[];
+        for(let c=0;c<columns;c++){
+            let cardImage =cardSet.pop();
+            row.push(cardImage);
+
+            let card=document.createElement("img");
+            card.id=r.toString()+"-"+c.toString();
+            card.src=cardImage+".jpg";
+            card.classList.add("card");
+            document.getElementById("board").append(card);
+        }
+        board.push(row);
+    }
+}
